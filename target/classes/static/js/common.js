29f1fef9af1a0794c1f67027d9cde4f2cd3a8069
765651375@qq.com <765651375@qq.com>
2018-05-18 13:30:45 +0800

    update .gitignore

--- target/classes/static/js/common.js
+++
@@ -1,7 +0,0 @@
-//Salt
-var password_salt = "z0fdf7f8g9o1";
-//展示loading
-function showLoading(){
-    var idx = layer.msg('处理中...', {icon: 16,shade: [0.5, '#f5f5f5'],scrollbar: false,offset: '0px', time:100000}) ;
-    return idx;
-}
