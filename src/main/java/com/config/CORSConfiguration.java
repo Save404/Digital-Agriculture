a170bd50c0b85819d90edc5c682a964c27631056
ZYSzys <17367077526@163.com>
2018-05-27 16:31:26 +0800

    :point_up: my backup

--- src/main/java/com/config/CORSConfiguration.java
+++ src/main/java/com/config/CORSConfiguration.java
@@ -11,7 +11,7 @@ public class CORSConfiguration {
 
     private CorsConfiguration buildConfig() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedOrigin("http://localhost:8081");
         corsConfiguration.addAllowedHeader("*");
         corsConfiguration.addAllowedMethod("*");
         corsConfiguration.setAllowCredentials(true);
